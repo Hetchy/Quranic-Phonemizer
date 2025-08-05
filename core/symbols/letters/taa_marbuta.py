05eb38e717cda4c295e29022172d762af3c58e71
Hetchy <ahmed.ibrahim8165@gmail.com>
2025-08-05 22:38:02 +1000

    taa marbuta

--- core/symbols/letters/taa_marbuta.py
+++ core/symbols/letters/taa_marbuta.py
@@ -2,5 +2,7 @@ from typing import List
 from .letter import LetterSymbol
 
 class TaaMarbutaLetter(LetterSymbol):
-    def phonemize(self) -> List[str]:
-        return []
+    def phonemize_letter(self) -> List[str]:
+        if self.is_last and self.has_sukun:
+            return ["h"]
+        return [self.base_phoneme]
